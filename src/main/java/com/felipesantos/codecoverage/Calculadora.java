7f53cde13146c5b22650cd8695d317bc02938ca0
Felipe Santos <felipecassiors@gmail.com>
2018-11-01 09:28:38 -0300

    Create packages

--- src/main/java/com/felipesantos/codecoverage/Calculadora.java
+++ src/main/java/com/felipesantos/codecoverage/Calculadora.java
@@ -1,3 +1,5 @@
+package com.felipesantos.codecoverage;
+
 public class Calculadora {
     public static int soma(int a, int b) {
         return a + b;
--- src/main/java/com/felipesantos/codecoverage/Main.java
+++ src/main/java/com/felipesantos/codecoverage/Main.java
@@ -1,3 +1,5 @@
+package com.felipesantos.codecoverage;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Teste!");
