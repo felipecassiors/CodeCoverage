69d8ff5deb7b5be26a35a8512a5e31baac16dca1
Felipe Santos <felipecassiors@gmail.com>
2018-11-01 09:06:12 -0300

    Initial commit

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,20 @@
+public class Calculadora {
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtracao(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicacao(int a, int b) {
+        return a * b;
+    }
+
+    public static double divisao(int a, int b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Divisão por zero!");
+        }
+        return a * 1.0 / b;
+    }
+}
